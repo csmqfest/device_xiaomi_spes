c78d4f3bdc2b209a506073219b81e7b288ca4f5f
ryyaap. (unlstedd) <rktdnt@proton.me>
2022-11-13 07:19:30 +0700

    spes: Update fingerprint into latest miui per-region build
    Signed-off-by: ryyaap. (unlstedd) <rktdnt@proton.me>

--- libinit/init_xiaomi_spes.cpp
+++ libinit/init_xiaomi_spes.cpp
@@ -16,7 +16,7 @@ static const variant_info_t spes_info = {
     .brand = "Redmi",
     .device = "spes",
     .model = "2201117TG",
-    .build_fingerprint = "Redmi/spes_global/spes:11/RKQ1.211001.001/V13.0.15.0.RGCMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/spes_global/spes:12/SKQ1.211103.001/V13.0.5.0.SGCMIXM:user/release-keys",
 
     .nfc = false,
 };
@@ -28,7 +28,7 @@ static const variant_info_t spes_in_info = {
     .brand = "Redmi",
     .device = "spes",
     .model = "2201117TI",
-    .build_fingerprint = "Redmi/spes_in/spes:11/RKQ1.211001.001/V13.0.10.0.RGCINXM:user/release-keys",
+    .build_fingerprint = "Redmi/spes_in/spes:12/SKQ1.211103.001/V13.0.1.0.SGCINXM:user/release-keys",
 
     .nfc = false,
 };
@@ -40,7 +40,7 @@ static const variant_info_t spesn_info = {
     .brand = "Redmi",
     .device = "spesn",
     .model = "2201117TY",
-    .build_fingerprint = "Redmi/spesn_global/spesn:11/RKQ1.211001.001/V13.0.11.0.RGCMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/spesn_global/spesn:12/SKQ1.211103.001/V13.0.5.0.SGKMIXM:user/release-keys",
 
     .nfc = true,
 };
